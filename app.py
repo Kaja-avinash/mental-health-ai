ed5f24cd4372b2f2f2654893a51a2253355a70d4
Avinash <167685448+Kaja-avinash@users.noreply.github.com>
2026-02-21 21:23:55 +0530

    Connect app to HuggingFace trained model

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ st.set_page_config(
     layout="wide"
 )
 
-MODEL_PATH = "saved_models/emotion_model"
+MODEL_PATH = "avinash19/mental-health-emotion-model"
 
 emotion_labels = ["sadness", "fear", "anger", "joy", "neutral"]
 
